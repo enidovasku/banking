3ada033592954fc10e17aeafc8ed843a8e846e67
Enido Vasku <enidovasku@gmail.com>
2021-02-15 14:06:48 +0100

    Added transactions for the account

--- app/accountHandler.go
+++ app/accountHandler.go
@@ -33,3 +33,26 @@ func (h AccountHandler) NewAccount(w http.ResponseWriter, r *http.Request) {
 
 	writeResponse(w, http.StatusCreated, account)
 }
+
+func (h AccountHandler) MakeTransaction(w http.ResponseWriter, r *http.Request) {
+	//Get account_id and customer_id from the URL
+	vars := mux.Vars(r)
+	account_id := vars["account_id"]
+	customer_id := vars["customer_id"]
+
+	var request dto.TransactionRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		writeResponse(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	request.AccountId = account_id
+	request.CustomerId = customer_id
+
+	//make transaction
+	account, appError := h.service.MakeTransaction(request)
+	if appError != nil {
+		writeResponse(w, appError.Code, appError.AsMessage())
+		return
+	}
+	writeResponse(w, http.StatusOK, account)
+}
--- domain/account.go
+++ domain/account.go
@@ -5,19 +5,27 @@ import (
 	"github.com/enidovasku/banking/errs"
 )
 
+const dbTSLayout = "2006-01-02 15:04:05"
+
 type Account struct {
-	AccountId   string
-	CustomerId  string
-	OpeningDate string
-	AccountType string
-	Amount      float64
-	Status      string
+	AccountId   string  `db:"account_id"`
+	CustomerId  string  `db:"customer_id"`
+	OpeningDate string  `db:"opening_date"`
+	AccountType string  `db:"account_type"`
+	Amount      float64 `db:"amount"`
+	Status      string  `db:"status"`
 }
 
 type AccountRepository interface {
 	Save(Account) (*Account, *errs.AppError)
+	FindBy(string) (*Account, *errs.AppError)
+	SaveTransaction(Transaction) (*Transaction, *errs.AppError)
 }
 
 func (a Account) ToNewAccountResponseDto() dto.NewAccountResponse {
 	return dto.NewAccountResponse{a.AccountId}
+}
+
+func (a Account) CanWithdraw(amount float64) bool {
+	return a.Amount >= amount
 }
